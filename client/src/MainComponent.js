c7a3a195c1d2020a75e020bf32333e127531ca35
FabricioSarti <98360524+FabricioSarti@users.noreply.github.com>
2023-10-12 08:30:48 -0600

    se cargan archivos de docker para levantar proyecto

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -1,13 +1,47 @@
 import axios from "axios";
-import { useCallback } from "react";
+import { useCallback, useState, useEffect } from "react";
+import "./MainComponent.css"
 const MainComponent = () => {
 
-    const getAllNumbers = useCallback(() => {
+    const [values, setValues] = useState([])
+    const [value, setValue] = useState("")
 
-    })
+    const getAllNumbers = useCallback(async () => {
+        //usar nginx para redireccionar correctamente
+        const data = await axios.get('/api/values/values/all');
+        setValues(data.data.rows.map(row => row.number));
+    }, [])
+
+    const saveNumbers = useCallback(async (event) => {
+        event.preventDefault();
+
+        await axios.post('/api/values', {
+            value
+        })
+
+        setValue('');
+        getAllNumbers();
+
+    }, [value, getAllNumbers])
+
+    useEffect(() => {
+        getAllNumbers();
+    }, []);
+
 
     return (<div>
-        <button onClick={getAllNumbers()}>Obtener todos los numeros</button>
+        <button onClick={getAllNumbers}>Obtener todos los numeros</button>
+        <span className="title">Valores</span>
+        <div className="values">
+            {
+                values.map((value) => <div className="value">{value}</div>)
+            }
+        </div>
+        <form className="form" onSubmit={saveNumbers}>
+            <label>Ingrese un valor: </label>
+            <input value={value} onChange={((event) => { setValue(event.target.value) })} />
+            <button>Ok</button>
+        </form>
     </div>)
 }
 
